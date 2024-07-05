c239efaca7cf215812c43552bdba3b903253802e
Aman Welderufael <100647139+AmanWelderufael@users.noreply.github.com>
2024-07-04 20:59:57 -0400

    All fixed

--- src/test/java/com/TaskManagement/demo/DemoApplicationTests.java
+++ src/test/java/com/TaskManagement/demo/DemoApplicationTests.java
@@ -2,12 +2,17 @@ package com.TaskManagement.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-class DemoApplicationTests {
+@TestPropertySource(properties = {
+		"spring.datasource.url=jdbc:postgresql://localhost:5432/t_management_db",
+		"spring.datasource.username=postgres",
+		"spring.datasource.password=postgres1"
+})
+public class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
